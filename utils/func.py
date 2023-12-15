18713f37cc4844cd164c94a14e61d83d33d4f5e9
koipish <koipish@gmail.com>
2023-12-15 21:09:21 +0300

    added functions

---
+++ utils/func.py
@@ -0,0 +1,11 @@
+def number_format(name):
+    if "счет" in name.lower():
+        format_number = name[:4] + " " + "**" + name[-4:]
+        return format_number
+    else:
+        format_number = name[-len(name):-16] + " " + name[-16:-12] + " " + name[-12:-10] + "**" + " " + "****" + " " + name[-4:]
+        return format_number
+
+def data_format(date):
+    format_date = ".".join(date.split("-")[::-1])
+    return format_date
