8582af30a6e55724205b3dcfecc4f9203792f080
koipish <koipish@gmail.com>
2023-12-15 21:11:20 +0300

    added code

---
+++ utils/show_operation.py
@@ -0,0 +1,30 @@
+import json
+from func import number_format, data_format
+
+with open("C:/Users/User/Nika/course_work_proj/utils/operations.json", encoding='utf-8') as file:
+    data_dict = json.load(file)
+
+date_list = []
+for data in data_dict:
+    if "from" not in data:
+        continue
+    elif data["state"] == "EXECUTED":
+        date_list.append(data["date"][0:10])
+    sorted_list = sorted(date_list, reverse=True)[0:5]
+
+for i in range(5):
+     for data in data_dict:
+         if "date" not in data or "from" not in data:
+             continue
+         elif data["date"][0:10] == sorted_list[i]:
+             date_name = data["date"][0:10]
+             description_name = data["description"]
+             from_name = data["from"]
+             to_name = data["to"]
+             amount = data["operationAmount"]["amount"]
+             currency = data["operationAmount"]["currency"]["name"]
+
+             print(f"{data_format(date_name)} {description_name}\n"
+                   f"{number_format(from_name)} -> {number_format(to_name)}\n"
+                   f"{amount} {currency}\n"
+                   f"")
