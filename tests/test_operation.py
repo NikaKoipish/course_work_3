d83f957b52124baf96734926c08ec86a70661d37
koipish <koipish@gmail.com>
2023-12-16 19:36:02 +0300

    added test for get_filtered_data

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -17,3 +17,50 @@ def test_get_sorted_list():
     data = ["2018-03-23", "2019-08-26", "2019-07-03", "2020-07-03", "2014-07-03"]
     assert get_sorted_list(data) == ["2020-07-03", "2019-08-26", "2019-07-03", "2018-03-23", "2014-07-03"]
 
+def test_get_filtered_data():
+    dictionary = [
+  {
+    "id": 441945886,
+    "state": "CANCELLED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  },
+  {
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "to": "Счет 35383033474447895560"
+  },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {
+                "amount": "8221.37",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560"
+        }]
+    assert get_filtered_data(dictionary) == ["2019-07-03"]
