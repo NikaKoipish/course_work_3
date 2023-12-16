c801b658faebfdbeccdc621daaaf6625386f3e28
koipish <koipish@gmail.com>
2023-12-16 18:43:14 +0300

    added get_data and get_filtered_data

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,18 @@
+import json
 
+def get_data(path):
+    with open(path, encoding='utf-8') as file:
+        data_dict = json.load(file)
+        return data_dict
+
+def get_filtered_data(data_dict):
+    date_list = []
+    for data in data_dict:
+        if "from" not in data:
+            continue
+        elif data["state"] == "EXECUTED":
+            date_list.append(data["date"][0:10])
+    return date_list
 
 def number_format(name):
     if "счет" in name.lower():
