0b89e2614903386c5ae2b35bfc26b8ac1c7b726b
koipish <koipish@gmail.com>
2023-12-16 18:36:21 +0300

    changed number_format

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,13 @@
 
 def number_format(name):
     if "счет" in name.lower():
-        format_number = name[:4] + " " + "**" + name[-4:]
+        name_list = name.split()
+        format_number = name_list[0] + " " + "**" + name_list[-1][-4:]
         return format_number
     else:
-        format_number = name[-len(name):-16] + " " + name[-16:-12] + " " + name[-12:-10] + "**" + " " + "****" + " " + name[-4:]
+        name_list = name.split()
+        name_operation = name_list[0:-1]
+        format_number = " ".join(name_operation) + " " + name_list[-1][0:4] + " " + name_list[-1][4:6] + "**" + " " + "****" + " " + name_list[-1][-4:]
         return format_number
 
 def date_format(date):
